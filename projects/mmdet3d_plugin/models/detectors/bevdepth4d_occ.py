1209c7cfcd92fdfab5cbbdf69c1fdb3514fdbd59
SPA-junghokim <junghokim@spa.hanyang.ac.kr>
2024-03-20 02:47:02 +0900

    s5

---
+++ projects/mmdet3d_plugin/models/detectors/bevdepth4d_occ.py
@@ -0,0 +1,246 @@
+from ...ops import TRTBEVPoolv2
+from .bevdet import BEVDet
+from .bevdepth4d import BEVDepth4D
+from mmdet3d.models import DETECTORS
+from mmdet3d.models.builder import build_head
+import torch.nn.functional as F
+import torch.nn as nn
+
+
+@DETECTORS.register_module()
+class BEVDepth4DOCC_MTL(BEVDepth4D):
+    def __init__(self,
+                 occ_head=None,
+                 upsample=False,
+                 down_sample_for_3d_pooling=None,
+                 single_bev_num_channels=None,
+                 pc_range = [-40.0, -40.0, -1, 40.0, 40.0, 5.4],
+                 grid_size = [200, 200, 16],
+                 
+                 **kwargs):
+        super(BEVStereo4DOCC, self).__init__(**kwargs)
+        self.occ_head = build_head(occ_head)
+        self.pts_bbox_head = None
+        self.upsample = upsample
+
+    def forward_train(self,
+                      points=None,
+                      img_metas=None,
+                      gt_bboxes_3d=None,
+                      gt_labels_3d=None,
+                      gt_labels=None,
+                      gt_bboxes=None,
+                      img_inputs=None,
+                      proposals=None,
+                      gt_bboxes_ignore=None,
+                      **kwargs):
+        """Forward training function.
+
+        Args:
+            points (list[torch.Tensor], optional): Points of each sample.
+                Defaults to None.
+            img_metas (list[dict], optional): Meta information of each sample.
+                Defaults to None.
+            gt_bboxes_3d (list[:obj:`BaseInstance3DBoxes`], optional):
+                Ground truth 3D boxes. Defaults to None.
+            gt_labels_3d (list[torch.Tensor], optional): Ground truth labels
+                of 3D boxes. Defaults to None.
+            gt_labels (list[torch.Tensor], optional): Ground truth labels
+                of 2D boxes in images. Defaults to None.
+            gt_bboxes (list[torch.Tensor], optional): Ground truth 2D boxes in
+                images. Defaults to None.
+            img (torch.Tensor optional): Images of each sample with shape
+                (N, C, H, W). Defaults to None.
+            proposals ([list[torch.Tensor], optional): Predicted proposals
+                used for training Fast RCNN. Defaults to None.
+            gt_bboxes_ignore (list[torch.Tensor], optional): Ground truth
+                2D boxes in images to be ignored. Defaults to None.
+
+        Returns:
+            dict: Losses of different branches.
+        """
+        # img_feats: List[(B, C, Dz, Dy, Dx)/(B, C, Dy, Dx) , ]
+        # pts_feats: None
+        # depth: (B*N_views, D, fH, fW)
+        img_feats, pts_feats, depth = self.extract_feat(
+            points, img_inputs=img_inputs, img_metas=img_metas, **kwargs)
+
+        gt_depth = kwargs['gt_depth']   # (B, N_views, img_H, img_W)
+        losses = dict()
+        loss_depth = self.img_view_transformer.get_depth_loss(gt_depth, depth)
+        losses['loss_depth'] = loss_depth
+
+        voxel_semantics = kwargs['voxel_semantics']     # (B, Dx, Dy, Dz)
+        mask_camera = kwargs['mask_camera']     # (B, Dx, Dy, Dz)
+        loss_occ = self.forward_occ_train(img_feats[0], voxel_semantics, mask_camera)
+        losses.update(loss_occ)
+        return losses
+
+    def forward_occ_train(self, img_feats, voxel_semantics, mask_camera):
+        """
+        Args:
+            img_feats: (B, C, Dz, Dy, Dx) / (B, C, Dy, Dx)
+            voxel_semantics: (B, Dx, Dy, Dz)
+            mask_camera: (B, Dx, Dy, Dz)
+        Returns:
+        """
+        outs = self.occ_head(img_feats)
+        assert voxel_semantics.min() >= 0 and voxel_semantics.max() <= 17
+        loss_occ = self.occ_head.loss(
+            outs,  # (B, Dx, Dy, Dz, n_cls)
+            voxel_semantics,  # (B, Dx, Dy, Dz)
+            mask_camera,  # (B, Dx, Dy, Dz)
+        )
+        return loss_occ
+
+    def simple_test(self,
+                    points,
+                    img_metas,
+                    img=None,
+                    rescale=False,
+                    **kwargs):
+        # img_feats: List[(B, C, Dz, Dy, Dx)/(B, C, Dy, Dx) , ]
+        # pts_feats: None
+        # depth: (B*N_views, D, fH, fW)
+        img_feats, _, _ = self.extract_feat(
+            points, img_inputs=img, img_metas=img_metas, **kwargs)
+
+        occ_list = self.simple_test_occ(img_feats[0], img_metas)    # List[(Dx, Dy, Dz), (Dx, Dy, Dz), ...]
+        return occ_list
+
+    def simple_test_occ(self, img_feats, img_metas=None):
+        """
+        Args:
+            img_feats: (B, C, Dz, Dy, Dx) / (B, C, Dy, Dx)
+            img_metas:
+
+        Returns:
+            occ_preds: List[(Dx, Dy, Dz), (Dx, Dy, Dz), ...]
+        """
+        outs = self.occ_head(img_feats)
+        occ_preds = self.occ_head.get_occ(outs, img_metas)      # List[(Dx, Dy, Dz), (Dx, Dy, Dz), ...]
+        return occ_preds
+
+    def forward_dummy(self,
+                      points=None,
+                      img_metas=None,
+                      img_inputs=None,
+                      **kwargs):
+        # img_feats: List[(B, C, Dz, Dy, Dx)/(B, C, Dy, Dx) , ]
+        # pts_feats: None
+        # depth: (B*N_views, D, fH, fW)
+        img_feats, pts_feats, depth = self.extract_feat(
+            points, img_inputs=img_inputs, img_metas=img_metas, **kwargs)
+        occ_bev_feature = img_feats[0]
+        if self.upsample:
+            occ_bev_feature = F.interpolate(occ_bev_feature, scale_factor=2,
+                                            mode='bilinear', align_corners=True)
+        outs = self.occ_head(occ_bev_feature)
+        return outs
+    
+    def extract_img_feat(self,
+                         img_inputs,
+                         img_metas,
+                         pred_prev=False,
+                         sequential=False,
+                         **kwargs):
+        """
+        Args:
+            img_inputs:
+                imgs:  (B, N, 3, H, W)        # N = 6 * (N_history + 1)
+                sensor2egos: (B, N, 4, 4)
+                ego2globals: (B, N, 4, 4)
+                intrins:     (B, N, 3, 3)
+                post_rots:   (B, N, 3, 3)
+                post_trans:  (B, N, 3)
+                bda_rot:  (B, 3, 3)
+            img_metas:
+            **kwargs:
+        Returns:
+            x: [(B, C', H', W'), ]
+            depth: (B*N_views, D, fH, fW)
+        """
+        if sequential:
+            return self.extract_img_feat_sequential(img_inputs, kwargs['feat_prev'])
+        imgs, sensor2keyegos, ego2globals, intrins, post_rots, post_trans, \
+        bda, _ = self.prepare_inputs(img_inputs)
+
+        """Extract features of images."""
+        bev_feat_list = []
+        depth_list = []
+        key_frame = True  # back propagation for key frame only
+        for img, sensor2keyego, ego2global, intrin, post_rot, post_tran in zip(
+                imgs, sensor2keyegos, ego2globals, intrins, post_rots, post_trans):
+            if key_frame or self.with_prev:
+                if self.align_after_view_transfromation:
+                    sensor2keyego, ego2global = sensor2keyegos[0], ego2globals[0]
+
+                mlp_input = self.img_view_transformer.get_mlp_input(
+                    sensor2keyegos[0], ego2globals[0], intrin, post_rot, post_tran, bda)    # (B, N_views, 27)
+
+                inputs_curr = (img, sensor2keyego, ego2global, intrin, post_rot,
+                               post_tran, bda, mlp_input)
+                if key_frame:
+                    # bev_feat: (B, C, Dy, Dx)
+                    # depth: (B*N_views, D, fH, fW)
+                    bev_feat, depth = self.prepare_bev_feat(*inputs_curr)
+                else:
+                    with torch.no_grad():
+                        bev_feat, depth = self.prepare_bev_feat(*inputs_curr)
+            else:
+                # https://github.com/HuangJunJie2017/BEVDet/issues/275
+                bev_feat = torch.zeros_like(bev_feat_list[0])
+                depth = None
+            bev_feat_list.append(bev_feat)
+            depth_list.append(depth)
+            key_frame = False
+
+        # bev_feat_list: List[(B, C, Dy, Dx), (B, C, Dy, Dx), ...]
+        # depth_list: List[(B*N_views, D, fH, fW), (B*N_views, D, fH, fW), ...]
+
+        if pred_prev:
+            assert self.align_after_view_transfromation
+            assert sensor2keyegos[0].shape[0] == 1      # batch_size = 1
+            feat_prev = torch.cat(bev_feat_list[1:], dim=0)
+            # (1, N_views, 4, 4) --> (N_prev, N_views, 4, 4)
+            ego2globals_curr = \
+                ego2globals[0].repeat(self.num_frame - 1, 1, 1, 1)
+            # (1, N_views, 4, 4) --> (N_prev, N_views, 4, 4)
+            sensor2keyegos_curr = \
+                sensor2keyegos[0].repeat(self.num_frame - 1, 1, 1, 1)
+            ego2globals_prev = torch.cat(ego2globals[1:], dim=0)            # (N_prev, N_views, 4, 4)
+            sensor2keyegos_prev = torch.cat(sensor2keyegos[1:], dim=0)      # (N_prev, N_views, 4, 4)
+            bda_curr = bda.repeat(self.num_frame - 1, 1, 1)     # (N_prev, 3, 3)
+            return feat_prev, [imgs[0],     # (1, N_views, 3, H, W)
+                               sensor2keyegos_curr,     # (N_prev, N_views, 4, 4)
+                               ego2globals_curr,        # (N_prev, N_views, 4, 4)
+                               intrins[0],          # (1, N_views, 3, 3)
+                               sensor2keyegos_prev,     # (N_prev, N_views, 4, 4)
+                               ego2globals_prev,        # (N_prev, N_views, 4, 4)
+                               post_rots[0],    # (1, N_views, 3, 3)
+                               post_trans[0],   # (1, N_views, 3, )
+                               bda_curr]        # (N_prev, 3, 3)
+
+        if self.align_after_view_transfromation:
+            for adj_id in range(1, self.num_frame):
+                bev_feat_list[adj_id] = self.shift_feature(
+                    bev_feat_list[adj_id],  # (B, C, Dy, Dx)
+                    [sensor2keyegos[0],     # (B, N_views, 4, 4)
+                     sensor2keyegos[adj_id]     # (B, N_views, 4, 4)
+                    ],
+                    bda     # (B, 3, 3)
+                )   # (B, C, Dy, Dx)
+
+        bev_feat = torch.cat(bev_feat_list, dim=1)      # (B, N_frames*C, Dy, Dx)
+        
+        
+        if self.down_sample_for_3d_pooling is not None:
+            # B, N, sem_C, sem_H, sem_W = x.shape
+            # x = x.view(B,N*sem_C,sem_H, sem_W)
+            x = self.down_sample_for_3d_pooling(x)
+            # x = self.embed(x)
+        
+        x = self.bev_encoder(bev_feat)
+        return [x], depth_list[0]
+
+
